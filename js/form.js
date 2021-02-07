921903e29a1d1c84e4d9aaab3551a3cbf8657a2a
Shokhboz Abdullaev <abdullaevadiana27@gmail.com>
2021-02-07 22:35:00 +0300

    'mobile-80%'

--- js/form.js
+++ js/form.js
@@ -1,11 +1,44 @@
 const form = document.querySelector(".inputs")
 const successMsg = document.querySelector(".success-msg")
+const inputs = form.querySelectorAll("input, select")
+const smErrorMsg = document.querySelectorAll(".small-error-hint")
 
+form.addEventListener("submit", (e) => {
+	e.preventDefault()
+	let noErrors = true
+	inputs.forEach((i) => {
+		console.log(i)
+		if (!noErrors) {
+			return
+		}
+		if (i.value.trim() !== "") {
+			noErrors = true
+			return
+		}
+		noErrors = false
+	})
 
+	if (noErrors) {
+		form.classList.add("d-none")
+		successMsg.classList.remove("d-none")
+		successMsg.classList.add("d-block")
+	} else {
+		inputs.forEach((i, index) => {
+			if (i.value.trim() === "") {
+				console.log(i)
+				i.classList.add("form-control-error")
+				smErrorMsg[index].classList.add("small-error-hint-shown")
+			}
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    form.classList.add('d-none')
-    successMsg.classList.remove('d-none')
-    successMsg.classList.add('d-block')
+			if (i.value.trim() !== "") {
+				i.classList.remove("form-control-error")
+				smErrorMsg[index].classList.remove("small-error-hint-shown")
+			}
+
+			if (i.id === "phone" ? i.value.trim() === "+7" : false) {
+				i.classList.add("form-control-error")
+				smErrorMsg[index].classList.add("small-error-hint-shown")
+			}
+		})
+	}
 })
