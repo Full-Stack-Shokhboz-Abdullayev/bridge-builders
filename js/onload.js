5084637e8ebb225f3dfc85aae88ff64b774b1698
Shokhboz Abdullaev <abdullaevadiana27@gmail.com>
2021-03-05 14:26:41 +0300

    'autoplay-attempt'

--- js/onload.js
+++ js/onload.js
@@ -1,4 +1,26 @@
+Object.defineProperty(HTMLMediaElement.prototype, "playing", {
+	get: function () {
+		return !!(
+			this.currentTime > 0 &&
+			!this.paused &&
+			!this.ended &&
+			this.readyState > 2
+		)
+	}
+})
+
 window.addEventListener("load", async function () {
 	const video = document.querySelector("#autoplay")
 	await video.play()
+	if (!video.playing) {
+		const interval = setInterval(async () => {
+			await video.play()
+			if (video.playing) {
+				clearInterval(interval)
+				console.log("Video is playing.")
+			}
+		}, 1000)
+	} else {
+		console.log("Video is playing.")
+	}
 })
